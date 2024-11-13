9b2817fedc624ead53b28d3f821d153e05e21dcd
lsdvitamin <lsdvitamin7@gmail.com>
2024-11-13 20:36:59 +0300

    init

---
+++ src/main/java/ru/otus/homework6/lesson16/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.homework6.lesson16;
+
+/**
+ * @author Sergei on 13.11.2024 20:29.
+ * @project Default (Template) Project
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
